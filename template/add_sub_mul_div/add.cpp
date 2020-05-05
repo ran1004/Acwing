efef99a5564f068bdcdd6b13733630f5353f53bf
ran1004 <phr610600@163.com>
2020-05-05 20:00:30 +0800

    frist commit

---
+++ template/add_sub_mul_div/add.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> add(vector<int>& A, vector<int> &B) {
+    int t = 0;
+    vector<int> C;
+    for (int i = 0; i < A.size() || i < B.size(); i++) {
+        if (i < A.size()) t += A[i];
+        if (i < B.size()) t += B[i];
+        C.push_back(t % 10);
+        t /= 10;
+    }
+    if (t) C.push_back(t);
+    return C;
+}
+
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    vector<int> A;
+    vector<int> B;
+    for (int i = a.size()-1; i >= 0; i--)  A.push_back(a[i] - '0');
+    for (int i = b.size()-1; i >= 0; i--)  B.push_back(b[i] - '0');
+    vector<int> C = add(A, B);
+    for (int i = C.size() - 1; i >= 0; i--) cout << C[i];
+    cout << endl;
+    return 0;
+}
